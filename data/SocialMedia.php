b99aea5675c98fa11cdfee166acca00f825fac2c
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-01 08:48:31 +0700

    add SocialMedia.php

---
+++ data/SocialMedia.php
@@ -0,0 +1,17 @@
+<?php
+  class SocialMedia
+  {
+    public string $name;
+  }
+
+  final class Facebook extends SocialMedia
+  {
+
+  }
+
+  //error
+  
+  // class FakeFacebook extends Facebook
+  // {
+  //
+  // }
