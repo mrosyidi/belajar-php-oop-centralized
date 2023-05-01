dbb36ba19b7363f8a0e7391c5b5e1cfc1f5570e2
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-01 19:30:44 +0700

    update ObjectIterator.php iterator

--- ObjectIteration.php
+++ ObjectIteration.php
@@ -1,10 +1,22 @@
 <?php
-  class Data
+  class Data implements IteratorAggregate
   {
     var string $first = "First";
     public string $second = "Second";
     public string $third = "Third";
     protected string $fourth = "Fourth";
+
+    public function getIterator()
+    {
+      $array = [
+        "first" => $this->first,
+        "second" => $this->second,
+        "third" => $this->third,
+        "fourth" => $this->fourth
+      ];
+
+      return new ArrayIterator($array);
+    }
   }
 
   $data = new Data();
