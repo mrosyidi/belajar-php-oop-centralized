9b623f5f52802b5d0dc9087866f74330952480d2
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-06 13:39:48 +0700

    update Covariance.php contravariance

--- Covariance.php
+++ Covariance.php
@@ -1,8 +1,11 @@
 <?php
   require_once "data/Animal.php";
   require_once "data/AnimalShelter.php";
+  require_once "data/Food.php";
 
   $catShelter = new \Data\CatShelter();
   $cat = $catShelter->adopt("Luna");
+  $cat->eat(new \Data\AnimalFood());
   $dogShelter = new \Data\DogShelter();
   $dog = $dogShelter->adopt("Doggy");
+  $cat->eat(new \Data\Food());
