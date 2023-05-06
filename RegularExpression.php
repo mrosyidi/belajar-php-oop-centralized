8d343e16829fed8517773aa8deecc19fa3b5ebe1
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-06 18:24:38 +0700

    update RegularExpression.php replace

---
+++ RegularExpression.php
@@ -0,0 +1,8 @@
+<?php
+  $matches = [];
+  $result = (bool)preg_match_all("/eko|awan|edy/i", "Eko Kurniawan Khannedy", $matches);
+  var_dump($result);
+  var_dump($matches);
+
+  $result = preg_replace("/anjing|bangsat/i", "***", "dasar lu anjing dan bangsat!");
+  var_dump($result);
